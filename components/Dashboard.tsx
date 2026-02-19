d45b05591b07f7dc083647042282b9336445249b
Noah Kjos <noahkjos8@gmail.com>
2026-02-18 20:52:03 -0700

    Create Dashboard component

---
+++ components/Dashboard.tsx
@@ -0,0 +1,11 @@
+// Sample content to be replaced with actual content from jackguag-sketch/AI-college-councelling
+function Dashboard() {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {/* Additional content goes here */}
+        </div>
+    );
+}
+
+export default Dashboard;
