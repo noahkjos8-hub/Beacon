ca6be9aee8a095734dc2fa32db6243b38005bcb5
Noah Kjos <noahkjos8@gmail.com>
2026-02-18 20:51:30 -0700

    Create AuthView component in TypeScript

---
+++ components/AuthView.tsx
@@ -0,0 +1,13 @@
+// AuthView.tsx
+import React from 'react';
+
+const AuthView: React.FC = () => {
+    return (
+        <div>
+            <h1>Authentication View</h1>
+            {/* Add your authentication logic here */}
+        </div>
+    );
+};
+
+export default AuthView;
