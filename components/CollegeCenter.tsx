ce17edf2e894921edd485dddc62d5758514c27cd
Noah Kjos <noahkjos8@gmail.com>
2026-02-18 20:51:39 -0700

    Create CollegeCenter component

---
+++ components/CollegeCenter.tsx
@@ -0,0 +1,14 @@
+// Components/CollegeCenter.tsx
+
+import React from 'react';
+
+const CollegeCenter: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the College Center</h1>
+            <p>Your journey to academic success starts here!</p>
+        </div>
+    );
+};
+
+export default CollegeCenter;
