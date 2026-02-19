6f9d36fb1b9b28db9b4f2fcd85e9fa85cc2da258
Noah Kjos <noahkjos8@gmail.com>
2026-02-18 20:51:01 -0700

    Create ActivitiesResume component

---
+++ components/ActivitiesResume.tsx
@@ -0,0 +1,13 @@
+// ActivitiesResume.tsx
+import React from 'react';
+
+const ActivitiesResume = () => {
+    return (
+        <div>
+            <h1>Activities & Resume</h1>
+            {/* Add your content here */}
+        </div>
+    );
+};
+
+export default ActivitiesResume;
