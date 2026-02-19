51a9adb526a9d2fb5d82bbc14223a906b2b30eeb
Noah Kjos <noahkjos8@gmail.com>
2026-02-18 20:51:54 -0700

    Create CounselorChat.tsx component

---
+++ components/CounselorChat.tsx
@@ -0,0 +1,13 @@
+// Import necessary libraries and components
+import React from 'react';
+
+const CounselorChat = () => {
+    return (
+        <div>
+            <h1>Counselor Chat Component</h1>
+            {/* Add chat functionalities here */}
+        </div>
+    );
+};
+
+export default CounselorChat;
